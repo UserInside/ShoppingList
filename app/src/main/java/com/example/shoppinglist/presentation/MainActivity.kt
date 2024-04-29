3d25f823913fa8c07fca77254305bbb96449992c
Igor <voronov.i.v@mail.ru>
2024-04-29 22:19:02 +0300

    add fragment to MainActivity.kt in twoPaneMode

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.shoppinglist.presentation
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
